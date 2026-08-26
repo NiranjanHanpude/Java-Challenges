2d0e40252ade2b37bd9b8299ff22bd15b3716788
Niranjan Hanpude <niranjanhanpude2525@gmail.com>
2026-08-26 23:06:02 +0530

    initial comit

---
+++ src/76CarFinal.java
@@ -0,0 +1,12 @@
+ class CarFinal {
+    final int noOfWheels;
+    final String model;
+    final String engineinliters;
+
+
+     public CarFinal(int noOfWheels, String model, String engineinliters) {
+         this.noOfWheels = noOfWheels;
+         this.model = model;
+         this.engineinliters = engineinliters;
+     }
+ }
